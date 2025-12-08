5e40f92019363a1a8513e422a5cf37d5326a4321
Saiganity1 <sicatmichaeldave0411@gmail.com>
2025-12-09 00:01:32 +0800

    Fix view.py

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,24 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import HttpResponse
+from django.views.static import serve
+import os
 
 urlpatterns = [
     # Minimal health endpoint at root to satisfy PaaS probes and avoid 404 noise
     path('', lambda request: HttpResponse('ok')),
     path('admin/', admin.site.urls),
     path('api/', include('market.urls')),
+]
+
+# Temporary: serve media files at /static/media/... directly from MEDIA_ROOT.
+# This helps when running on platforms with ephemeral filesystems (like Render)
+# and when uploaded media needs to be accessible immediately. For production
+# use a proper object storage (S3) and remove this route.
+urlpatterns += [
+    re_path(r'^static/media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 if settings.DEBUG:
