3292c9633151a865944be5e0b4a6f012bac86401
Saiganity1 <sicatmichaeldave0411@gmail.com>
2025-12-08 23:38:32 +0800

    Fixed Not found in logs of Render

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
 urlpatterns = [
+    # Minimal health endpoint at root to satisfy PaaS probes and avoid 404 noise
+    path('', lambda request: HttpResponse('ok')),
     path('admin/', admin.site.urls),
     path('api/', include('market.urls')),
 ]
